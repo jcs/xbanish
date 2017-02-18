e8272aa525dfa3c2b2cd8a59a438d2c2c1020208
= <gislan@utumno.pl>
2017-02-18 23:07:30 +0100

    Hide cursor on touch events. Only supported in rawmode with XInput2.

--- xbanish.c
+++ xbanish.c
@@ -172,6 +172,12 @@ main(int argc, char *argv[])
 			case XI_RawButtonRelease:
 				break;
 
+            case XI_RawTouchBegin:
+            case XI_RawTouchEnd:
+            case XI_RawTouchUpdate:
+                hide_cursor();
+                break;
+
 			default:
 				if (debug)
 					printf("unknown XI event type %d\n",
@@ -247,6 +253,7 @@ snoop_xinput(Window win)
 
 		XISetMask(mask, XI_RawMotion);
 		XISetMask(mask, XI_RawButtonPress);
+		XISetMask(mask, XI_RawTouchBegin);
 		evmasks[0].deviceid = XIAllMasterDevices;
 		evmasks[0].mask_len = sizeof(mask);
 		evmasks[0].mask = mask;
