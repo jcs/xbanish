3dcc4484c0c8859fe1fe53cc0390ebb178411d51
Ckat <Ckat@teknik.io>
2022-02-18 15:02:34 +0000

    add -s option to ignore scrolling events (#62)

--- xbanish.c
+++ xbanish.c
@@ -49,7 +49,7 @@ static int device_change_type = -1;
 static long last_device_change = -1;
 
 static Display *dpy;
-static int hiding = 0, legacy = 0, always_hide = 0;
+static int hiding = 0, legacy = 0, always_hide = 0, ignore_scroll = 0;
 static unsigned timeout = 0;
 static unsigned char ignored;
 static XSyncCounter idler_counter = 0;
@@ -92,7 +92,7 @@ main(int argc, char *argv[])
 		{"all", -1},
 	};
 
-	while ((ch = getopt(argc, argv, "adi:m:t:")) != -1)
+	while ((ch = getopt(argc, argv, "adi:m:t:s")) != -1)
 		switch (ch) {
 		case 'a':
 			always_hide = 1;
@@ -131,6 +131,8 @@ main(int argc, char *argv[])
 			break;
 		case 't':
 			timeout = strtoul(optarg, NULL, 0);
+		case 's':
+			ignore_scroll = 1;
 			break;
 		default:
 			usage(argv[0]);
@@ -254,6 +256,9 @@ main(int argc, char *argv[])
 			switch (xie->evtype) {
 			case XI_RawMotion:
 			case XI_RawButtonPress:
+				if (ignore_scroll && ((xie->detail >= 4 && xie->detail <= 7) ||
+						xie->event_x == xie->event_y))
+					break;
 				if (!always_hide)
 					show_cursor();
 				break;
@@ -574,7 +579,7 @@ void
 usage(char *progname)
 {
 	fprintf(stderr, "usage: %s [-a] [-d] [-i mod] [-m [w]nw|ne|sw|se] "
-	    "[-t seconds]\n", progname);
+	    "[-t seconds] [-s]\n", progname);
 	exit(1);
 }
 
